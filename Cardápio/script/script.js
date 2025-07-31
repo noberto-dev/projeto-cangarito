2f07c08af704314b74234926afe046480afa9cf4
João Victor Noberto <santana.t@escolar.ifrn.edu.br>
2025-07-31 14:28:55 -0300

    cardapio v-alguma coisa
    transição alinhada

--- Cardápio/script/script.js
+++ Cardápio/script/script.js
@@ -4,11 +4,13 @@ const diasSemanaExpand = document.getElementById('dias-semana-expand')
 diasSemanaMinimized.addEventListener('click', () =>{
     const aberto = diasSemanaExpand.classList.toggle('open');
     
+    tempoTimeout = aberto ? 10 : 300
+    let timeoutIDAberto = setTimeout(() => {
     if (aberto){
-        diasSemanaMinimized.style.borderRadius = '16px 16px 0 0'
+        diasSemanaMinimized.style.borderRadius = '16px 16px 0 0';
+    }else {
+        diasSemanaMinimized.style.borderRadius = '16px';
     }
-    else{
-        diasSemanaMinimized.style.borderRadius = '16px'
-    }
+    }, tempoTimeout);
 })
 
